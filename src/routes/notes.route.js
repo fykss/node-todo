21ae825cad77e92bb6e45556bd41e1b415a67d1b
Влад Летун <fyks1649@gmail.com>
2019-05-24 21:39:05 +0300

    notes routes

--- src/routes/notes.route.js
+++ src/routes/notes.route.js
@@ -3,7 +3,11 @@ const { Router } = require('express');
 const router = new Router();
 
 router.get('/', function (req, res) {
-    res.render('Index/Index');
+    res.render('Notes/Notes');
+});
+
+router.get('/:id', function (req, res) {
+    res.render('Notes/Notes');
 });
 
 module.exports = router;
