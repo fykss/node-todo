8aec28e31eb14722bef0013265cba58b352c2883
Dima Onopko <onopko24@gmail.com>
2019-05-24 22:50:02 +0300

    add back listNode

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+//ROUTES
+const indexRouter = require('./src/routes/index.route');
+const listsRouter = require('./src/routes/lists.route');
+const apiRouter = require('./src/routes/api/lists.route');
+
 const { PORT, MONGODB_URL } = require('./config');
 
 const app = express();
 
-app.set('views', './src/templates');
+app.set('views', './src/templates/views');
 app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname,'/public')));
+app.use(indexRouter);
+app.use('/lists', listsRouter);
+app.use('/api', apiRouter);
 
-
-
-
-
-
-
-
-app.listen(PORT, err => err ? console.error(err) : console.log(`We are on http://localhost:${PORT}`));
-
+mongoose.connect( MONGODB_URL , { useNewUrlParser: true });
+mongoose.connection.on('error' , console.error.bind(console, 'db connection err'));
+mongoose.connection.once('open', function callback(){
+    app.listen(PORT, err => err ? console.error(err) : console.log(`We are on http://localhost:${PORT}`));
+});
